85cf266d9c30aa048aa1dd2a5d9c772b154e6495
Your Name <majorearthquake@163.com>
2024-11-24 18:32:39 +0800

    Initial commit

--- src/main/java/com/freefish/torchesbecomesunlight/server/entity/dialogueentity/IDialogue.java
+++
@@ -1,7 +0,0 @@
-package com.freefish.torchesbecomesunlight.server.entity.dialogueentity;
-
-import com.freefish.torchesbecomesunlight.server.story.dialogue.Dialogue;
-
-public interface IDialogue {
-    public Dialogue getDialogue();
-}
